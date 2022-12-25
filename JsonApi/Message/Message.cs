bdf2ff406f0dbaa4d9b0dde4021176dbc4812174
Never <kovalev080903@mail.ru>
2022-12-25 20:47:41 +0300

    Docker added

---
+++ JsonApi/Message/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace JsonApi.Message
+{
+    public class Message
+    {
+        // класс сообщения статуса сервера
+        public class ServerStatus
+        {
+            public string Status { get; set; }
+            public DateTime DateTime { get; set; }
+            public string HostName { get; set; }
+            public ServerStatus(string status, DateTime dateTime, string hostName)
+            {
+                Status = status;
+                DateTime = dateTime;
+                HostName = hostName;
+            }
+        }
+    }
+}
+
--- JsonApi/Program.cs
+++ JsonApi/Program.cs
@@ -1,3 +1,5 @@
+using JsonApi.Message;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -8,11 +10,12 @@ app.MapGet("/ping", async (context) =>
 });
 app.MapGet("/status", async (context) =>
 {
-    await context.Response.WriteAsync(Convert.ToString(System.Diagnostics.Process.GetCurrentProcess()));
+    Message.ServerStatus serverStatus = new Message.ServerStatus("OK", DateTime.Now, System.Net.Dns.GetHostName());
+    await context.Response.WriteAsJsonAsync(serverStatus);
 });
 app.MapGet("/info", async (context) =>
 {
-
+    await context.Response.WriteAsync("Methods: /ping(checking the work), /status(info about process)");
 });
 
 app.Run();
