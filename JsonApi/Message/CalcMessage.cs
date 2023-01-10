16f40404733aaf04bfe0cb08239666b6f0731d95
Never <kovalev080903@mail.ru>
2023-01-10 18:16:50 +0300

    +docker

---
+++ JsonApi/Message/CalcMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonApi.Message
+{
+    public class CalcMessage
+    {
+        public record CalcInput(double a, double b, double c);
+        public record CalcOutput(double x1, double x2);
+    }
+}
---
+++ JsonApi/Services/CalcService.cs
@@ -0,0 +1,22 @@
+﻿using static JsonApi.Message.CalcMessage;
+namespace JsonApi.Services
+
+{
+    public class CalcService
+    {
+        public CalcOutput SolutionEquation(CalcInput input)
+        {
+            CalcOutput output = null;
+            double a = input.a;
+            double b = input.b;
+            double c = input.c;
+            if ((b*b-4*a*c) >= 0) //Если дискриминант больше или равен 0
+            {
+                double x1 = (-1 * b + Math.Sqrt(((b * b - 4 * a * c))) / (2 * a));
+                double x2 = (-1 * b - Math.Sqrt(((b * b - 4 * a * c))) / (2 * a));
+                output = new CalcOutput(x1, x2);
+            }
+            return output;
+        }
+    }
+}
